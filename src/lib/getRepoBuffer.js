96e2c0a89fd48682356b164135d4ea91352d4f00
alina sireneva <alina@tei.su>
2024-11-29 19:15:40 +0300

    feat: support for did:web

--- src/lib/getRepoBuffer.js
+++ src/lib/getRepoBuffer.js
@@ -2,9 +2,14 @@ import { AtpAgent } from "@atproto/api";
 
 const getRepoBuffer = async (did) => {
   let pds = undefined;
-  const service = (
-    await (await fetch(`https://plc.directory/${did}`)).json()
-  ).service.find((service) => service.id === "#atproto_pds");
+  let didDocument = undefined;
+  if (did.indexOf("did:plc:") === 0) {
+    didDocument = await (await fetch(`https://plc.directory/${did}`)).json()
+  } else if (did.indexOf("did:web:") === 0) {
+    didDocument = await (await fetch(`https://${did.replace("did:web:", "")}/.well-known/did.json`)).json()
+  }
+
+  const service = didDocument.service.find((service) => service.id === "#atproto_pds");
   if (service && service.serviceEndpoint) pds = service.serviceEndpoint;
   if (pds === undefined) throw new Error("Could not resolve PDS.");
 
