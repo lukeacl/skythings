6e1f47e7705567f4a038c209477a400880a4a2f4
lukeacl <hello@lukeacl.com>
2025-10-05 11:27:26 +1000

    Commit.

--- src/pages/BloomfieBubble.jsx
+++ src/pages/BloomfieBubble.jsx
@@ -13,6 +13,7 @@ import getFollowingInteractionCounts from "../lib/getFollowingInteractionCounts"
 import getRepoBuffer from "../lib/getRepoBuffer";
 
 import downloadIcon from "../icons/download-duotone-solid.svg";
+import copyIcon from "../icons/copy-duotone-solid.svg";
 
 import bubbleImage from "../images/bubble.png";
 
@@ -29,6 +30,14 @@ const showError = (message) => {
   });
 };
 
+const showInfo = (title, message) => {
+  Swal.fire({
+    icon: "info",
+    title: title,
+    text: message,
+  });
+};
+
 function BloomfieBubble() {
   const [handle, setHandle] = createSignal("");
   const [handleGenerated, setHandleGenerated] = createSignal("");
@@ -36,9 +45,10 @@ function BloomfieBubble() {
   const [percentLoaded, setPercentLoaded] = createSignal(0);
   const [image, setImage] = createSignal("");
   const [isChartVisible, setIsChartVisible] = createSignal(false);
-  const [timePeriod, setTimePeriod] = createSignal(_SECONDS_ALL_TIME);
+  const [timePeriod, setTimePeriod] = createSignal(_SECONDS_ONE_MONTH);
   const [size, setSize] = createSignal(2048);
   const [count, setCount] = createSignal(100);
+  const [altText, setAltText] = createSignal("");
 
   const generate = async () => {
     if (handle().trim() === "")
@@ -127,11 +137,19 @@ function BloomfieBubble() {
       const avatarPromises = [];
       let avatarPromisesCompleted = 0;
 
+      let newAltText = `A bubble on a blue background containing a circular avatar of @${handle()} in the middle, then arranged around @${handle()} more circular avatars of people @${handle()} interacts with the most. The size of the circlular avatars are getting smaller as you move around and outwards representing less and less interaction. Avatars in order from most interaction to least interaction include `;
+      let altTextFooter = `. A credit at the bottom right reads ${handle()} #BloomfieBubble skythings.lukeacl.com.`;
+
       for (const [index, child] of (root.children || []).entries()) {
         ctx.beginPath();
         ctx.arc(child.x + padding, child.y + padding, child.r, 0, 2 * Math.PI);
         ctx.fillStyle = color(index) + "aa";
         ctx.fill();
+
+        let altTextAddition = `${index === 0 ? `@${child.data.handle}` : `, @${child.data.handle}`}`;
+        if (`${newAltText}${altTextAddition}${altTextFooter}`.length <= 2000) {
+          newAltText = `${newAltText}${altTextAddition}`;
+        }
 
         if (child.data.avatar) {
           try {
@@ -189,6 +207,10 @@ function BloomfieBubble() {
         }
       }
 
+      newAltText = `${newAltText}${altTextFooter}`;
+
+      setAltText(newAltText.slice(0, 2000));
+
       await Promise.all(avatarPromises);
 
       await new Promise(async (resolve) => {
@@ -209,11 +231,77 @@ function BloomfieBubble() {
         img.src = bubbleImage;
       });
 
+      const creditURLText = "skythings.lukeacl.com";
+      ctx.font = `${Math.floor(size() / 60)}px sans-serif`;
+      ctx.fillStyle = "#ffffff99";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "bottom";
+      const creditURLTextMetrics = ctx.measureText(creditURLText);
+      ctx.fillText(
+        creditURLText,
+        size() - creditURLTextMetrics.width - padding / 2,
+        size() - creditURLTextMetrics.actualBoundingBoxAscent - padding / 2,
+      );
+
+      const titleText = `#BloomfieBubble`;
+      ctx.font = `bold ${Math.floor(size() / 60)}px sans-serif`;
+      ctx.fillStyle = "#ffffff99";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "bottom";
+      const titleTextMetrics = ctx.measureText(titleText);
+      ctx.fillText(
+        titleText,
+        size() - titleTextMetrics.width - padding / 2,
+        size() -
+          creditURLTextMetrics.actualBoundingBoxAscent -
+          2 -
+          titleTextMetrics.actualBoundingBoxAscent -
+          padding / 2,
+      );
+
+      const timePeriodText = `${getTimePeriodLabel(timePeriod())}`;
+      ctx.font = `${Math.floor(size() / 60)}px sans-serif`;
+      ctx.fillStyle = "#ffffff99";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "bottom";
+      const timePeriodTextMetrics = ctx.measureText(timePeriodText);
+      ctx.fillText(
+        timePeriodText,
+        size() - timePeriodTextMetrics.width - padding / 2,
+        size() -
+          titleTextMetrics.actualBoundingBoxAscent -
+          2 -
+          creditURLTextMetrics.actualBoundingBoxAscent -
+          2 -
+          timePeriodTextMetrics.actualBoundingBoxAscent -
+          padding / 2,
+      );
+
+      const handleText = `@${handle()}`;
+      ctx.font = `${Math.floor(size() / 60)}px sans-serif`;
+      ctx.fillStyle = "#ffffff99";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "bottom";
+      const handleTextMetrics = ctx.measureText(handleText);
+      ctx.fillText(
+        handleText,
+        size() - handleTextMetrics.width - padding / 2,
+        size() -
+          timePeriodTextMetrics.actualBoundingBoxAscent -
+          2 -
+          titleTextMetrics.actualBoundingBoxAscent -
+          2 -
+          creditURLTextMetrics.actualBoundingBoxAscent -
+          2 -
+          handleTextMetrics.actualBoundingBoxAscent -
+          padding / 2,
+      );
+
       await new Promise((resolve) => {
         canvas.toBlob((blob) => {
           try {
-            const blobUrl = URL.createObjectURL(blob);
-            setImage(blobUrl);
+            const blobURL = URL.createObjectURL(blob);
+            setImage(blobURL);
             setHandleGenerated(handle());
           } catch (error) {
           } finally {
@@ -227,6 +315,14 @@ function BloomfieBubble() {
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const copyAltText = async () => {
+    navigator.clipboard.writeText(altText());
+    showInfo(
+      "Copied!",
+      "The alt text for this Bloomfie Bubble has been copied to your clipboard.",
+    );
   };
 
   const download = async () => {
@@ -255,65 +351,34 @@ function BloomfieBubble() {
 
   return (
     <>
-      <p class="mb-2 font-semibold">Who do you interact with the most?</p>
+      <p class="font-semibold mb-2">Who do you interact with the most?</p>
       {image() && (
         <>
           <span
             id="chartWrapper"
-            class="flex flex-col justify-center items-center bg-sky-300 mb-4"
+            class="flex flex-col justify-center items-center bg-sky-300 mb-2"
           >
-            <span id="chart" class="flex justify-center mb-2">
+            <span id="chart" class="flex justify-center">
               <img src={image()} style="max-width: 50vw; height: auto;" />
             </span>
-            <p class="text-xs font-light opacity-50 text-center">
-              {getTimePeriodLabel(timePeriod())} #bloomfiebubble for @
-              {handleGenerated()}
-            </p>
-            <p class="text-xs font-extralight opacity-40 text-center">
-              Generate yours at
-              <br />
-              {window.location.href}
-            </p>
           </span>
-          {true && (
+          <span class="flex flex-row">
             <a
+              download={"bloomfiebubble.png"}
               href={image()}
               target="_blank"
               onClick={download}
-              class="flex flex-row items-center text-sm bg-sky-200 p-2 rounded mb-6 hover:opacity-80"
+              class="flex flex-row items-center text-sm bg-sky-200 p-2 rounded mb-6 hover:opacity-80 mr-2"
             >
               <img src={downloadIcon} width="16" class="mr-2" /> Download
             </a>
-          )}
-        </>
-      )}
-      {isChartVisible() && (
-        <>
-          <span
-            id="chartWrapper"
-            class="flex flex-col justify-center items-center bg-sky-300 p-4"
-          >
-            <span id="chart" class="flex justify-center mb-2"></span>
-            <p class="text-xs font-light opacity-50 text-center">
-              {getTimePeriodLabel(timePeriod())} #bloomfiebubble for @
-              {handleGenerated()}
-            </p>
-            <p class="text-xs font-extralight opacity-40 text-center">
-              Generate yours at
-              <br />
-              {window.location.href}
-            </p>
+            <button
+              onClick={copyAltText}
+              class="flex flex-row items-center text-sm bg-sky-200 p-2 rounded mb-6 hover:opacity-80"
+            >
+              <img src={copyIcon} width="16" class="mr-2" /> Copy Alt Text
+            </button>
           </span>
-          {true && (
-            <a
-              href={image()}
-              target="_blank"
-              onClick={download}
-              class="flex flex-row items-center text-sm bg-sky-200 p-2 rounded mb-6 hover:opacity-80"
-            >
-              <img src={downloadIcon} width="16" class="mr-2" /> Download
-            </a>
-          )}
         </>
       )}
       {isLoading() && (
@@ -321,7 +386,7 @@ function BloomfieBubble() {
           <LoadingMessage />
         </span>
       )}
-      <span>
+      <span class="mt-2 mb-2">
         <input
           autocomplete="off"
           autocorrect="off"
@@ -349,7 +414,7 @@ function BloomfieBubble() {
           {isLoading() && percentLoaded() > 0 ? ` (${percentLoaded()}%)` : ""}
         </button>
       </span>
-      <span class="mt-2">
+      <span class="">
         <button
           class={
             "p-1 rounded text-xs mr-2 " +
